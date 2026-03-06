1eaf5cba9dc3d73a7826d873b7a64109be976144
Nicolas Gomez <nicolasgomez94@gmail.com>
2026-03-06 15:54:38 -0300

    reorden de nav. con subdivisiones

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,8 +16,57 @@ const Navbar = () => {
         
         <ul className="navbar-menu">
           <li><Link to="/">Inicio</Link></li>
-          <li><Link to="/coordinacion">Coordinación</Link></li>
-          <li><Link to="/recursos">Recursos</Link></li>
+          
+          <li className="dropdown">
+            <Link to="/coordinacion">Coordinación</Link>
+            <ul className="dropdown-menu">
+              <li className="dropdown-group-title">Operación</li>
+              <li><Link to="/coordinacion#operacion">Planteles</Link></li>
+              <li><Link to="/coordinacion#operacion">Asistencia</Link></li>
+              <li><Link to="/coordinacion#operacion">Reunión de padres</Link></li>
+              <li><Link to="/coordinacion#operacion">Distribución de espacios</Link></li>
+              
+              <li className="dropdown-divider"></li>
+              <li className="dropdown-group-title">Competencia</li>
+              <li><Link to="/coordinacion#competencia">Categoría E1</Link></li>
+              <li><Link to="/coordinacion#competencia">Categoría 4ta B</Link></li>
+              <li><Link to="/coordinacion#competencia">Fixture</Link></li>
+              <li><Link to="/coordinacion#competencia">Videos de partidos</Link></li>
+              <li><Link to="/coordinacion#competencia">Videos de rivales</Link></li>
+              
+              <li className="dropdown-divider"></li>
+              <li className="dropdown-group-title">Temporada 2026</li>
+              <li><Link to="/coordinacion#temporada-2026">Plan de pretemporada</Link></li>
+              <li><Link to="/coordinacion#temporada-2026">Inicio de actividades</Link></li>
+              <li><Link to="/coordinacion#temporada-2026">Pretemporada de febrero</Link></li>
+              
+              <li className="dropdown-divider"></li>
+              <li className="dropdown-group-title">Gestión interna</li>
+              <li><Link to="/coordinacion#gestion-interna">Entrenadores</Link></li>
+              <li><Link to="/coordinacion#gestion-interna">Árbitros</Link></li>
+              <li><Link to="/coordinacion#gestion-interna">Fotos</Link></li>
+              <li><Link to="/coordinacion#gestion-interna">Capacitaciones</Link></li>
+            </ul>
+          </li>
+          
+          <li className="dropdown">
+            <Link to="/recursos">Recursos</Link>
+            <ul className="dropdown-menu">
+              <li className="dropdown-group-title">Planificación deportiva</li>
+              <li><Link to="/recursos#planificacion-deportiva">Modelo de juego</Link></li>
+              <li><Link to="/recursos#planificacion-deportiva">Fases de la planificación</Link></li>
+              <li><Link to="/recursos#planificacion-deportiva">Plan de acción - Diagnóstico</Link></li>
+              <li><Link to="/recursos#planificacion-deportiva">Plan de acción - Objetivos</Link></li>
+              
+              <li className="dropdown-divider"></li>
+              <li className="dropdown-group-title">Entrenamientos</li>
+              <li><Link to="/recursos#entrenamientos">Simbología</Link></li>
+              <li><Link to="/recursos#entrenamientos">Planilla de entrenamiento</Link></li>
+              <li><Link to="/recursos#entrenamientos">Planilla de partido</Link></li>
+              <li><Link to="/recursos#entrenamientos">Guía de gestos técnicos</Link></li>
+            </ul>
+          </li>
+          
           <li><Link to="/contacto">Contacto</Link></li>
           <li><Link to={panelPath}>Panel</Link></li>
         </ul>
